0c53670c5eb0e0fb533d4472d1ae4280f5f84578
rikutokd <rikutokd@yahoo.co.jp>
2024-05-18 23:10:05 +0900

    add 音声認識

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import PySimpleGUI as sg
+import requests
+import os
+from dotenv import load_dotenv
 from PIL import Image
+
+load_dotenv()
 
 # 画像をリサイズする関数
 def resize_image(input_image_path, output_image_path, size):
@@ -9,14 +14,51 @@ def resize_image(input_image_path, output_image_path, size):
 
 # 画像のリサイズ
 resize_image('./images/play.png', './images/play_resized.png', (70, 50))
-resize_image('./images/stop.png', './images/stop_resized.png', (40, 40))
-resize_image('./images/maid.png', './images/maid_resized.png', (400, 400))
+resize_image('./images/maid.png', './images/maid_resized.png', (300, 300))
+
+# 環境変数からAPP_KEYを取得する
+APP_KEY = os.getenv('APP_KEY')
+
+URL = "https://acp-api.amivoice.com/v1/recognize"
+
+# 認識対象の音声ファイルパス
+AUDIO_FILE = "test.wav"
+
+def play_action():
+    # 音声ファイルを開く
+    with open(AUDIO_FILE, "rb") as f:
+        audio_data = f.read()
+
+    # リクエストのパラメータを設定
+    params = {
+        "d": "-a-general"
+    }
+    files = {
+        "u": (None, APP_KEY),
+        "a": ("test.wav", audio_data)
+    }
+
+    # POSTリクエストを送信
+    response = requests.post(URL, params=params, files=files)
+
+    # レスポンスを処理
+    if response.status_code == 200:
+        result = response.json()
+        return result["text"]
+    else:
+        return "エラー: " + response.text
 
 # レイアウト
 layout = [
-    [sg.Image(filename='./images/maid_resized.png', key='-IMAGE-')],
-    [sg.Image(filename='./images/play_resized.png', key='-PLAY-', enable_events=True), 
-     sg.Image(filename='./images/stop_resized.png', key='-STOP-', enable_events=True)]
+    [
+        sg.Frame('AIメイド', layout=[
+            [sg.Image(filename='./images/maid_resized.png', key='-IMAGE-')],
+            [sg.Multiline(size=(50, 10), key='-LOG-', autoscroll=True, disabled=True)]
+        ]),
+        sg.Column([
+            [sg.Button(image_filename='./images/play_resized.png', key='-PLAY-', enable_events=True, border_width=0, pad=(20, 0))]
+        ])
+    ]
 ]
 
 # ウィンドウ作成
@@ -29,7 +71,8 @@ while True:
         break
     elif event == '-PLAY-':
         print('再生ボタンがクリックされました')
-    elif event == '-STOP-':
-        print('停止ボタンがクリックされました')
+        # Curlプロセスを実行
+        result_text = play_action()
+        window['-LOG-'].print(result_text)
 
 window.close()
